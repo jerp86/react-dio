542e1614ebd64186bee7708ba6e688c22092447b
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-22 23:00:41 -0300

    criando a estrutura do componente Card

---
+++ src/components/Card/index.js
@@ -0,0 +1,26 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        Meu primeiro card
+      </div>
+      <div className="card-body">
+        <button
+          type="button"
+          className="btn btn-success"
+        >
+          Adicionar
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+        >
+          Remover
+        </button>
+        <p>0</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
