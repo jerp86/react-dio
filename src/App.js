fbf7b2fb1f44c1a7106a8e2116f30aae4ca7e047
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-22 21:55:12 -0300

    Utilizando o React.Fragment para ter mais de 1 elemento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </>
   );
 };
 
