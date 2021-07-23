ebaf6b94a89433c809ba95d9ff719e9a4c0e682b
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-22 23:04:29 -0300

    criando e utilizando funções modificadoras do estado

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,6 +3,14 @@ import { useState } from 'react';
 const Card = () => {
   const [valor, setValor] = useState(0);
 
+  function Adicionar() {
+    setValor(valor + 1);
+  }
+
+  function Remover() {
+    setValor(valor - 1);
+  }
+
   return (
     <div className="card">
       <div className="card-header">
@@ -12,12 +20,14 @@ const Card = () => {
         <button
           type="button"
           className="btn btn-success"
+          onClick={Adicionar}
         >
           Adicionar
         </button>
         <button
           type="button"
           className="btn btn-danger"
+          onClick={Remover}
         >
           Remover
         </button>
