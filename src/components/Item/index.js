a5fc3bfd1bb3a0782e543a30b7a7fbf290a8872c
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-22 22:06:20 -0300

    criando o primeiro componente

---
+++ src/components/Item/index.js
@@ -0,0 +1,12 @@
+const Item = () => {
+  return (
+    <a
+      href="/"
+      className="list-group-item list-group-item-action list-group-item-success"
+    >
+      Item 1
+    </a>
+  );
+};
+
+export default Item;
